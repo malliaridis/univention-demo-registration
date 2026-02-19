a633c0fd1be8f33b7f1e3cc13c7e09bc0fa6d199
Christos Malliaridis <c.malliaridis@gmail.com>
2026-02-19 17:59:22 +0200

    Support development mode with in-memory database for development and tests

--- server/src/main/kotlin/com/malliaridis/univention/database/Constants.kt
+++ server/src/main/kotlin/com/malliaridis/univention/database/Constants.kt
@@ -20,9 +20,34 @@ internal const val DATABASE_URL = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1"
 internal const val H2_DATABASE_DRIVER = "org.h2.Driver"
 
 /**
+ * Database driver for PostgreSQL database.
+ */
+internal const val POSTGRES_DATABASE_DRIVER = "org.postgresql.Driver"
+
+/**
  * Max character limit for varchar table columns.
  *
  * This serves as a general-purpose length, and is not intended for production.
  * TODO Use more realistic length based on the input fields.
  */
-const val MAX_VARCHAR_LENGTH = 128
+internal const val MAX_VARCHAR_LENGTH = 128
+
+/**
+ * Database connection URL environment variable.
+ */
+internal const val DATABASE_JDBC_URL_KEY = "DB_JDBC_URL"
+
+/**
+ * Database driver environment variable.
+ */
+internal const val DATABASE_DRIVER_KEY = "DB_DRIVER"
+
+/**
+ * Database user environment variable.
+ */
+internal const val DATABASE_USER_KEY = "DB_USER"
+
+/**
+ * Database password environment variable.
+ */
+internal const val DATABASE_PASSWORD_KEY = "DB_PASSWORD"
